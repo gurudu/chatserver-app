2c9b8b6bbb6242ddfeedae5d7a6b8914daf1772c
Hari kumar Gurudu <gharikumar2@gmail.com>
2018-01-10 20:04:30 +0100

    Added code for User Form validation  and added some styling

--- client/chat.js
+++ client/chat.js
@@ -6,14 +6,35 @@ $(function () {
 	      output = $('#output'),
 	      userForm = $('#user-form'),
 	     userError= $('#user-error'),
-	      userName = $('#UserName');
+	      userName = $('#UserName'),
+	      chatBox = $('#chat-box');
 
+	let validate = function(){	
+		let pattern = /[^a-z|^A-Z|^\s]/;
+		if(userName.val().match(pattern)){
+		   userError.empty();
+		   userError.html(`<p style="color:red">Please enter alphabets and space only!</p>`);
+		   userName.focus();
+		  return false;
+		}	
+	  return true;
+	}
 	//Emit events
 	userForm.on("submit", (e) => {
-		   e.preventDefault();
+		e.preventDefault();
+		if(validate()){
 			console.log(userName.val());
-	    	socket.emit('new user', userName.val());
-	    
+	    	socket.emit('new user', userName.val(), function(data){
+	               	 if(data){
+	               	 	$('#userLogin').hide();
+	                    chatBox.show();
+	                    
+	               	 }else{
+	               	 	userError.empty();
+	               	 	userError.html('<p style="color:red">This user name is already taken! Try again</p>');
+	               	 }
+		    });
+	    }
 	});      
 
     messageForm.on('submit', (e) => {
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const socket = require('socket.io');
 const io = socket(http);
 
 let port = process.env.PORT || 3000;
-
+let onlineUsers = [];
 
 http.listen(port, () => {
   console.log(`server is listening for requests on port ${port}`);
@@ -22,9 +22,29 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
    console.log('a user connected');
    
-   socket.on('new user', (data) => socket.userName = data );
+   socket.on('new user', (data, callback) => {
+  	if(onlineUsers.indexOf(data) !== -1){
+  		callback(false);
+  	  } else {
+  	  	callback(true);
+  	  	socket.userName = data;
+  	  	onlineUsers.push(socket.userName);
+        updateOnlineUsers();
+  	  }
+    });
 
    socket.on('send chat', (data) => {
-        socket.broadcast.emit('new chat', { userName:socket.userName, msg:data });
+        socket.broadcast.emit('new chat',{userName:socket.userName, msg:data});
       });
+
+    socket.on('disconnect', () => {
+   	console.log('a user disconnected', socket.id);
+   	if(!socket.userName) return;
+     onlineUsers.splice(onlineUsers.indexOf(socket.userName),1);
+     updateOnlineUsers();
+   });
+
+   function updateOnlineUsers(){
+   	 io.emit('online users', onlineUsers);
+   }
 });
